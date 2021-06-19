c4c95bea8b4545a7166c9e9596743fb3ddfc75df
ichux <theichux@gmail.com>
2021-06-19 05:41:58 +0100

    dev branch @ 2021-06-19T04:41:58Z Listed below are the changes in this commit:
    ==-==
    modified:   entrypoint.sh
    1. Added a little hack to ensure migrations are run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 set -e
 
-LOCATION=/var/www/migrations
-
 # prime the DB to be sure that it is up and running
-# python3.7 prime_db.py
 printf "import os\nimport time\n\nimport psycopg2\n\n\ndef primes():\n    \
 def connect():\n        connection = None\n        \
 host, database = os.getenv('POSTGRES_HOST'), os.getenv('POSTGRES_DB')\n        \
@@ -23,19 +20,10 @@ connection = psycopg2.connect(\n                \
  break\n\n        print('sleeping for 3s..')\n        time.sleep(3)\n\n\nprimes()" \
  | /usr/bin/env python3 && echo "DB is Up"
 
-# initialise the migration if it hasn't been done
-# if [ ! -d "$LOCATION" ]; then
-#   flask dbi
-# fi
+# a hack to run migrations
+printf "import os; os.system('flask dbi; flask dbm; flask dbu-no-sql')" \
+  | /usr/bin/env python3
 
-# # run the migration if there is one in existence
-# if [ -d "$LOCATION" ]; then
-#   flask dbm;
-
-#   if flask dbu-no-sql; then
-#     echo database migrations have been run!
-#   fi
-# fi
 
 if [ "$1" = 'supervisord' ]; then
     exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
