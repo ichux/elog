ee3efa393ea7badafe7401149241606fb93a6cc8
Hans Tognon <learningway403@gmail.com>
2022-04-22 14:57:37 +0100

    wip: filling new UI with data

--- elog/static-generator/src/elog.ts
+++ elog/static-generator/src/elog.ts
@@ -2,43 +2,64 @@ import Alpine from "alpinejs";
 import { Grid } from "gridjs";
 import { RowSelection } from "gridjs/plugins/selection";
 import fakeData from "./faker";
-import './gridjs.css'
+import "./gridjs.css";
 
 Alpine.data("elog", () => ({
+  csrf: "",
   grid: undefined as Grid | undefined,
   updateSelectionState(state: any, previousState: any) {
     console.log("Update: ", state, previousState);
   },
   onGridReady() {
     const checkboxPlugin = this.grid.config.plugin.get("checkboxes");
-    checkboxPlugin.props.store.on("updated", this.updateSelectionState);
+    checkboxPlugin.props.store.on(
+      "updated",
+      this.updateSelectionState.bind(this)
+    );
+  },
+  initCsrf() {
+    const meta: HTMLMetaElement | null = document.querySelector(
+      "meta[name='csrf-token']"
+    );
+    this.csrf = meta?.content ?? "";
   },
   async loadData() {
-    return new Promise((resolve, _) => {
-      resolve(fakeData.data.map((record: any) => {
-        return [
-          record.id,
-          record.code,
-          record.httpmethod,
-          // record.errormsg,
-          // record.errortraceback,
-          record.errortype,
-          record.ip,
-          record.postvalues,
-          record.referrer,
-          record.requestargs,
-          record.requestpath,
-          record.useragent,
-          record.userbrowser,
-          record.userbrowserversion,
-          record.userplatform,
-          record.when,
-        ];
-      }));
+    // return new Promise((resolve, _) => {
+    //   resolve(
+    //     fakeData.data.map((record: any) => {
+    //       return [
+    //         record.id,
+    //         record.code,
+    //         record.httpmethod,
+    //         // record.errormsg,
+    //         // record.errortraceback,
+    //         record.errortype,
+    //         record.ip,
+    //         record.postvalues,
+    //         record.referrer,
+    //         record.requestargs,
+    //         record.requestpath,
+    //         record.useragent,
+    //         record.userbrowser,
+    //         record.userbrowserversion,
+    //         record.userplatform,
+    //         record.when,
+    //       ];
+    //     })
+    //   );
+    // });
+    const response = await fetch("/data", {
+      headers: {
+        "X-CSRFToken": this.csrf,
+        "Content-Type": "application/json;charset=UTF-8",
+      },
     });
+    if (response.ok) {
+      console.log(await response.json());
+    }
   },
   async init(): Promise<void> {
-    console.log("Aloaaaa :)")
+    this.initCsrf();
     this.grid = new Grid({
       columns: [
         {
@@ -75,8 +96,6 @@ Alpine.data("elog", () => ({
         limit: 5,
         summary: false,
       },
-      sort: true,
-      resizable: true,
     });
     this.grid.on("ready", this.onGridReady.bind(this));
     this.grid.render(this.$refs["tableWrapper"]);
