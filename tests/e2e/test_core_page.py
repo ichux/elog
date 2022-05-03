b2415a275d19ea194f6d41f10a9542300933f369
Hans Tognon <learningway403@gmail.com>
2022-05-03 12:01:30 +0100

    feat: add test case for core page actions

--- tests/e2e/test_core_page.py
+++ tests/e2e/test_core_page.py
@@ -1,8 +1,10 @@
 from secrets import token_hex
-from flask_testing import LiveServerTestCase  # type: ignore
-from seleniumbase import BaseCase  # type: ignore
+
+import pytest
 import requests  # type: ignore
 from dotenv import load_dotenv
+from flask_testing import LiveServerTestCase  # type: ignore
+from seleniumbase import BaseCase  # type: ignore
 
 load_dotenv()
 
@@ -47,15 +49,48 @@ class TestCorePage(BaseCase, LiveServerTestCase):
 
     def test_records_shown(self):
         self.goto(self.get_server_url())
-        self.wait(5)
         self.reload()
-        self.assert_elements('table')
+        self.assert_elements('table', 'tr.gridjs-tr', 'td.gridjs-td')
 
+    @pytest.mark.skip(reason="Not fully functional yet")
     def test_record_details(self):
         self.goto(self.get_server_url())
         self.reload()
         self.assert_element('tr.gridjs-tr')
+        self.click('tr.gridjs-tr')
         row = self.get_element('tr.gridjs-tr')
         print(row)
         row.click()
         self.wait(12)
+
+    def test_action_select_all(self):
+        self.goto(self.get_server_url())
+        self.reload()
+        self.assert_element('#actions .dropdown button')
+        self.click('#actions .dropdown button')
+        self.click('#select-all')
+        checkboxes = self.find_elements('input[type="checkbox"]')
+        assert [c.is_selected() for c in checkboxes] == [True for _ in range(len(checkboxes))]
+
+    def test_action_unselect_all(self):
+        self.test_action_select_all()  # Because we need to check elements before
+        self.click('#actions .dropdown button')
+        self.click('#unselect-all')
+        checkboxes = self.find_elements('input[type="checkbox"]')
+        assert [c.is_selected() for c in checkboxes] == [False for _ in range(len(checkboxes))]
+
+    def test_toggle_action(self):
+        self.goto(self.get_server_url())
+        self.reload()
+        checkboxes = self.find_elements('input[type="checkbox"]')
+        checkboxes[0].click()
+        assert ([checkboxes[0].is_selected()], [c.is_selected() for c in checkboxes[1:]]) == (
+            [True], [False for _ in range(len(checkboxes) - 1)])
+        self.click('#actions .dropdown button')
+        self.click('#toggle-selection')
+        assert ([checkboxes[0].is_selected()], [c.is_selected() for c in checkboxes[1:]]) == (
+            [False], [True for _ in range(len(checkboxes) - 1)])
+
+    @pytest.mark.skip(reason="Find if there is a way to access clipboard content from Selenium")
+    def test_copy_as_csv_action(self):
+        ...
